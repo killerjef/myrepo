53908bb63f3102ff6d9d7b272648239d2d6f41bd
killerjef <jeferson12309@gmail.com>
2019-03-15 12:00:01 -0500

    php

---
+++ scripts_ejemplos/script1.php
@@ -0,0 +1,16 @@
+<center>programacion avanzada</center>
+<?php
+$nombre= "Peter";
+$apellido= "watson";
+$edad=30;
+$salario=800;
+
+
+echo "<center>BIENVENIDOS</center>";
+echo "Nombre: ". $nombre."<br>";
+echo "Apellido: ". $apellido."<br>";
+echo "Edad: ". $edad;
+echo "<br> salario: ". $salario;
+
+?>
+<center>Fin del programa</center>
---
+++ scripts_ejemplos/script2.php
@@ -0,0 +1,16 @@
+<?php
+$num1=10;
+$num2=5;
+
+echo "calculadora basica";
+
+/*$suma=$num1+$num2;*/
+$resta=$num1-$num2;
+$mult=$num1*$num2;
+$div=$num1/$num2;
+
+echo "<br>la suma es: ". ($num1+$num2);
+echo "<br>la resta es: ". $resta;
+echo "<br>la multiplicacion es: ". $mult;
+echo "<br>la division es: ". $div;
+?>
